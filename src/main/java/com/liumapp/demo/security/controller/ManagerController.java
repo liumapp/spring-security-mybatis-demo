f0b7315a9b1b29c0af353fad0f999778bacb25f9
liumapp <liumapp.com@gmail.com>
2018-02-02 16:50:50 +0800

    add different role controller

---
+++ src/main/java/com/liumapp/demo/security/controller/ManagerController.java
@@ -0,0 +1,9 @@
+package com.liumapp.demo.security.controller;
+
+/**
+ * Created by liumapp on 2/2/18.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+public class ManagerController {
+}

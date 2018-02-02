4b9006429cfecc292c15a71600492cc7f06d5eb0
liumapp <liumapp.com@gmail.com>
2018-02-02 15:06:59 +0800

    jwt user factory

--- src/main/java/com/liumapp/demo/security/auth/user/JwtUserFactory.java
+++ src/main/java/com/liumapp/demo/security/auth/user/JwtUserFactory.java
@@ -1,9 +1,13 @@
 package com.liumapp.demo.security.auth.user;
 
-import com.liumapp.demo.security.domain.User;
+import com.liumapp.demo.security.domain.*;
+import com.liumapp.demo.security.mapper.RoleMapper;
+import com.liumapp.demo.security.mapper.UserRoleMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,6 +17,21 @@ import java.util.stream.Collectors;
  * home-page:http://www.liumapp.com
  */
 public final class JwtUserFactory {
+
+    @Autowired
+    private UserRoleMapper userRoleMapper;
+
+    @Autowired
+    private RoleMapper roleMapper;
+
+    private static JwtUserFactory instance;
+
+    private static synchronized JwtUserFactory getInstance () {
+        if (instance == null) {
+            instance = new JwtUserFactory();
+        }
+        return instance;
+    }
 
     private JwtUserFactory() {
     }
@@ -25,15 +44,47 @@ public final class JwtUserFactory {
                 user.getEmail(),
                 user.getPhone(),
                 user.getPassword(),
-                mapToGrantedAuthorities(user.getAuthorities()),
-                user.getIsenabled(),
+                mapToGrantedAuthorities(JwtUserFactory.getInstance().getAuthorities(user)),
+                JwtUserFactory.getInstance().convert(user.getIsenabled()),
                 user.getType()
         );
     }
 
-    private static List<GrantedAuthority> mapToGrantedAuthorities(List<Authority> authorities) {
+    private static List<GrantedAuthority> mapToGrantedAuthorities(List<Role> authorities) {
         return authorities.stream()
                 .map(authority -> new SimpleGrantedAuthority(authority.getName().name()))
                 .collect(Collectors.toList());
     }
+
+    private List<Role> getAuthorities (User user) {
+        UserRoleExample userRoleExample = new UserRoleExample();
+        userRoleExample.createCriteria().andUseridEqualTo(user.getId());
+
+        LinkedList<UserRole> userRoles = (LinkedList<UserRole>) userRoleMapper.selectByExample(userRoleExample);
+        List<Role> roles = getRoles();
+
+        List<Role> result = new LinkedList<Role>();
+
+        while (userRoles.size() > 0) {
+            UserRole userRole = userRoles.pop();
+            for (int i = 0 ; i < roles.size() ; i++) {
+                Role role = roles.get(i);
+                if (role.getId().equals(userRole.getRoleid()))
+                    result.add(role);
+            }
+        }
+
+        return result;
+    }
+
+    private List<Role> getRoles () {
+        RoleExample roleExample = new RoleExample();
+        roleExample.createCriteria().andIdIsNotNull();
+        return roleMapper.selectByExample(roleExample);
+    }
+
+    private boolean convert (byte isenabled) {
+        return isenabled == 1 ;
+    }
+
 }

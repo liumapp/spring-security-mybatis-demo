098f50fbd0b22807972f24dc36af16e56658e2eb
liumapp <liumapp.com@gmail.com>
2018-02-02 16:57:06 +0800

    diff controller done

--- src/main/java/com/liumapp/demo/security/controller/BossController.java
+++ src/main/java/com/liumapp/demo/security/controller/BossController.java
@@ -1,9 +1,22 @@
 package com.liumapp.demo.security.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by liumapp on 2/2/18.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
+@RestController
+@RequestMapping("/boss")
 public class BossController {
+
+    @PreAuthorize("hasRole('BOSS')")
+    public ResponseEntity<?> getBossGreeting () {
+        return ResponseEntity.ok("Greeting from boss");
+    }
+
 }
